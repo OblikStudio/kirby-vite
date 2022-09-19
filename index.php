fd3fc8a83516e52c1c3caab772beffac010bf806
Johann Schopplich <mail@johannschopplich.com>
2022-09-19 08:00:17 +0200

    feat: use new default port 5173 for Vite 3

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 use Kirby\Cms\App;
-use Kirby\Http\Server;
+use Kirby\Http\Environment;
 use Oblik\KirbyVite\Vite;
 
 load([
@@ -11,16 +11,18 @@ load([
 /**
  * Returns the Vite singleton class instance.
  */
-function vite()
-{
-	return Vite::instance();
+if (!function_exists('vite')) {
+	function vite(): Vite
+	{
+		return Vite::instance();
+	}
 }
 
 App::plugin('oblik/vite', [
 	'options' => [
 		'server' => [
-			'host' => Server::host(),
-			'port' => 3000,
+			'host' => (new Environment())->host(),
+			'port' => 5173,
 			'https' => false,
 		],
 		'build' => [
